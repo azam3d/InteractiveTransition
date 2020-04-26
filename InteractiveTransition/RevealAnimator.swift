206e56b0311061a3a320493c17e69b36a2114553
Azam <swooshfx@gmail.com>
2020-04-26 09:35:50 +0800

    Fix indentations.

--- InteractiveTransition/RevealAnimator.swift
+++ InteractiveTransition/RevealAnimator.swift
@@ -7,79 +7,78 @@ enum TransitionType {
 }
 
 class RevealAnimator: UIPercentDrivenInteractiveTransition {
-  private var pausedTime: CFTimeInterval = 0
-  var transitionType = TransitionType.presenting
-  var interactive = false
-  let animationDuration = 0.2
+    private var pausedTime: CFTimeInterval = 0
+    var transitionType = TransitionType.presenting
+    var interactive = false
+    let animationDuration = 0.2
   
-  init(transitionType: TransitionType) {
-    self.transitionType = transitionType
-    
-    super.init()
-  }
+    init(transitionType: TransitionType) {
+        self.transitionType = transitionType
 
-  func handlePan(_ recognizer: UIPanGestureRecognizer) {
-    let translation = recognizer.translation(in: recognizer.view!.superview!)
-    var progress: CGFloat = translation.x / 400.0
-    
-    if progress > 0 {
-      return
-    } else {
-      progress = abs(progress)
+        super.init()
     }
-    progress = min(max(progress, 0.01), 0.99)
-    print(progress)
-    
-    switch recognizer.state {
-    case .changed:
-      update(progress)
-    case .cancelled, .ended:
-      if progress < 0.5 {
-        cancel()
-      } else {
-        finish()
-      }
-      interactive = false
-    default:
-      break
-    }
-  }
 
+    func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: recognizer.view!.superview!)
+        var progress: CGFloat = translation.x / 400.0
+        
+        if progress > 0 {
+            return
+        } else {
+            progress = abs(progress)
+        }
+        progress = min(max(progress, 0.01), 0.99)
+        print(progress)
+        
+        switch recognizer.state {
+        case .changed:
+            update(progress)
+        case .cancelled, .ended:
+            if progress < 0.5 {
+                cancel()
+            } else {
+                finish()
+            }
+            interactive = false
+        default:
+            break
+        }
+    }
 }
 
 extension RevealAnimator: UIViewControllerAnimatedTransitioning {
-  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-      return animationDuration
-  }
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
+    }
 
-  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-      let inView   = transitionContext.containerView
-      let toView   = transitionContext.view(forKey: .to)!
-      let fromView = transitionContext.view(forKey: .from)!
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let inView = transitionContext.containerView
+        let toView = transitionContext.view(forKey: .to)!
+        let fromView = transitionContext.view(forKey: .from)!
 
-      var frame = inView.bounds
+        var frame = inView.bounds
 
-      switch transitionType {
-      case .presenting:
-          frame.origin.x = -frame.size.width
-          toView.frame.origin = CGPoint(x: toView.frame.width, y: 0)
+        switch transitionType {
+        case .presenting:
+            frame.origin.x = -frame.size.width
+            toView.frame.origin = CGPoint(x: toView.frame.width, y: 0)
 
-          inView.addSubview(toView)
-          UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, options: .curveLinear, animations: {
-            toView.frame.origin = CGPoint(x: 0, y: toView.bounds.origin.y)
-          }, completion: { finished in
-              transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-          })
-      case .dismissing:
-          toView.frame = frame
-          inView.insertSubview(toView, belowSubview: fromView)
+            inView.addSubview(toView)
+            UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, options: .curveLinear, animations: {
+                toView.frame.origin = CGPoint(x: 0, y: toView.bounds.origin.y)
+            }, completion: { finished in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            })
+        case .dismissing:
+            toView.frame = frame
+            inView.insertSubview(toView, belowSubview: fromView)
 
-          UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, options: .curveLinear, animations: {
+            UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, options: .curveLinear, animations: {
             frame.origin.x = frame.width
-              fromView.frame = frame
-          }, completion: { finished in
-              transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-          })
-      }
+                fromView.frame = frame
+            }, completion: { finished in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            })
+        }
     }
 }
--- InteractiveTransition/ViewController.swift
+++ InteractiveTransition/ViewController.swift
@@ -4,27 +4,27 @@ import UIKit
 class ViewController: UIViewController {
   let transition = RevealAnimator(transitionType: .presenting)
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     
-    let pan = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
-    view.addGestureRecognizer(pan)
-  }
-  
-  @objc func didPan(_ recognizer: UIPanGestureRecognizer) {
-    switch recognizer.state {
-    case .began:
-      transition.interactive = true
-      
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      let vc = storyboard.instantiateViewController(withIdentifier: "Second") as! SecondViewController
-      vc.modalPresentationStyle = .fullScreen
-      vc.transitioningDelegate = self
-      present(vc, animated: true)
-    default:
-      transition.handlePan(recognizer)
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        view.addGestureRecognizer(pan)
     }
-  }
+  
+    @objc func didPan(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            transition.interactive = true
+      
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "Second") as! SecondViewController
+            vc.modalPresentationStyle = .fullScreen
+            vc.transitioningDelegate = self
+            present(vc, animated: true)
+        default:
+            transition.handlePan(recognizer)
+        }
+    }
 }
 
 extension ViewController: UIViewControllerTransitioningDelegate {
